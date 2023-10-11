a9ed9eac9d79c1382ae05cb856b8fef36e732847
JLGG4556 <jgongar564z@iesmarquesdecomares.org>
2023-10-11 19:01:59 +0200

    multiplicacion

--- Operacion_JLGG4556/src/Operaciones/Calculadora.java
+++ Operacion_JLGG4556/src/Operaciones/Calculadora.java
@@ -5,10 +5,10 @@ public class Calculadora {
 	public static void main(String[] args) {
 		int numero1 = 2;
 		int numero2 = 2;
-		int resultado;
+		int resultado, resultadomulti;
 		
 		resultado = numero1 + numero2;
-		
+		resultadomulti = numero1 * numero2;
 		
 
 	}
